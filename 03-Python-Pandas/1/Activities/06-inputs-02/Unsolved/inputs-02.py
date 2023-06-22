499b086b40918740d6cffca42206c89fad2ac013
Seshouan <session.mwamufiya@gmail.com>
2023-06-21 21:39:29 -0700

    completed a couple of the basic activities

--- 03-Python-Pandas/1/Activities/06-inputs-02/Unsolved/inputs-02.py
+++ 03-Python-Pandas/1/Activities/06-inputs-02/Unsolved/inputs-02.py
@@ -10,19 +10,24 @@ if name != "" and name != None:
     beverage = input("What kind of boba drink would you like?")
 
     # Then, Declare a variable of `sweetness` with an input and a string of "How sweet do you want your drink: 0, 50, 100, or 200?".
-
+    sweetness = input("How sweet do you want your drink: 0, 50, 100, or 200?")
 
     # If `sweetness` equals 50 print "half sweetened".
-
+    if sweetness == "50":
+        print("half sweetened")
     # Else if `sweetness` 100 print "normal sweet".
-
+    elif sweetness == "100":
+        print("normal sweetened")
     # Else if `sweetness` 200 print "super sweet".
-
+    elif sweetness == "200":
+        print("super sweetened")
     # Else print with a string of "non-sweet".
-
+    else:
+        print("non-sweet")
 
     # Then print the string of "Your order of " concatenated with the variable `beverage`, concatenated with " boba with a sweet level of ", concatenated with the variable `sweetness`
-
+    print(f"Your order of {beverage} boba with a sweet level of {sweetness}")
 # Else, print the string of "You didn't give us your name! Goodbye"
-
+else:
+    print("You didn't give us your name! Goodbye")
 
--- 03-Python-Pandas/1/Activities/08-dicts-01/Unsolved/dicts-01.py
+++ 03-Python-Pandas/1/Activities/08-dicts-01/Unsolved/dicts-01.py
@@ -1,29 +1,33 @@
 
-# san_francisco = {
-#     "west_coast": True,
-#     "has_multiple_bridges": True,
-#     "known_for_pizza": False,
-#     "coastal": True,
-#     "snows": False,
-#     "very_hot": False,
-#     "mayor": "London Breed",
-#     "state": "California",
-#     "country": "USA",
-#     "best_food": "burritos",
-#     "sports_teams": ["Giants", "Warriors", "Forty-Niners"],
-#     "tallest_building": "SalesForce Building",
-#     "population": 884363,
-#     "city_size": "large",
-#     "median_house_price": 1610000,
-#     "famous_residents": ["Maya Angelou", "Robert Frost", "Carlos Santana"],
-#     "homeless_pop": 1150,
-#     "political_leaning": "Democrat",
-#     "notable_attractions": ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"],
-#     "natural_disasters": ["Earthquakes"],
-# }
+san_francisco = {
+    "west_coast": True,
+    "has_multiple_bridges": True,
+    "known_for_pizza": False,
+    "coastal": True,
+    "snows": False,
+    "very_hot": False,
+    "mayor": "London Breed",
+    "state": "California",
+    "country": "USA",
+    "best_food": "burritos",
+    "sports_teams": ["Giants", "Warriors", "Forty-Niners"],
+    "tallest_building": "SalesForce Building",
+    "population": 884363,
+    "city_size": "large",
+    "median_house_price": 1610000,
+    "famous_residents": ["Maya Angelou", "Robert Frost", "Carlos Santana"],
+    "homeless_pop": 1150,
+    "political_leaning": "Democrat",
+    "notable_attractions": ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"],
+    "natural_disasters": ["Earthquakes"],
+}
 
 # Re-create the content of the commented out `san_francisco` dictionary by using bracket notation to manually add each of the key-value pairs (including nested objects).
+result_list = {}
 
+for key in san_francisco:
+    result_list[key] = san_francisco[key]
+print(result_list)
 
 
 
